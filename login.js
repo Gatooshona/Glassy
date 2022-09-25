0f71f39d610dddba33e5aa4e7eed3f0c64d62881
r2d2 <o.vinokurova@bk.ru>
2022-09-25 21:15:03 +0300

    Glassy init commit

---
+++ login.js
@@ -0,0 +1,14 @@
+var loginLink = document.querySelector(".login-link");
+var loginPopup = document.querySelector(".login");
+var loginClose = loginPopup.querySelector(".modal-close");
+var loginLogin = loginPopup.querySelector(".")
+
+loginLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    loginPopup.classList.add("modal-show");
+});
+
+loginClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    loginPopup.classList.remove("modal-show")
+})
